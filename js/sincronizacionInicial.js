82dbc96669332abcad6dd0fc6e20725de6ebbf66
gerionangel <gerion_angel@hotmail.com>
2014-11-13 10:31:02 +0100

    test
    intento de borrar el canvas loader con una promesa

--- js/sincronizacionInicial.js
+++ js/sincronizacionInicial.js
@@ -1,85 +1,92 @@
-(function (){
-    if(getData('primerEventoCoachMark')!= null && getData('primerEventoCoachMark')!=1){ //para que solo se llame una sola vez
+(function () {
+    if (getData('primerEventoCoachMark') != null && getData('primerEventoCoachMark') != 1) { //para que solo se llame una sola vez
         var auth = make_base_auth("app", "Kurbana2k14");
         $.ajaxSetup({
             headers: {
                 'Authorization': auth,
             }
         });
-        $.getJSON(sessionPath + "proyecto/proyectogetall?id=" + sessionProyecto, null, function (data) {
-            /*lugares*/
-            insertLugares(data.lugares);
-            /*Participantes*/
-            insertParticipantes(data.participantes);
-            /*Banners*/
-            insertBannersEvento(data.banners);
-            /*etiqutas*/
-            insertEtiquetas(data.etiquetas);
-            /*patrocinadores*/
-            insertPatrocinadores(data.patrocinadores);
-            /*tematicas*/
-            insertTematicas(data.tematicas) ;
-            /*nomenclatura filtros*/
-            insertNomenclaturaMenu(data.nomenclaturaFiltro);
-            /*nomenclatura Actividad*/
-            insertNomenclaturaActividad(data.nomenclaturaActividad);
-            /*eventos solo sincro*/
-            insertEventosSincroIncial(data.eventos);
-            /*actividades solo sincro*/
-            insertActividadesSoloSincro(data.actividades);
-            /*relacion actividad etiquetas*/
-            insertActividadEtiquetaSincroInicial(data.actEti);
-            /*relacion actividad-participante*/
-            insertActividadParticipanteSincroInicial(data.actPart);
-            /*relacion actividad-tematica*/
-            insertActividadTematicaSincroInicial(data.actTematica);
-            /*relacion actividad-evento*/
-            insertActividadEventoSincroInicial(data.actEvento);
-            /**/
-        });
+        var PromesaSincro = Promise(function (tengoSincro, errorSincro) {
+            $.getJSON(sessionPath + "proyecto/proyectogetall?id=" + sessionProyecto, null, function (data) {
+                /*lugares*/
+
+                insertLugares(data.lugares);
+                /*Participantes*/
+                insertParticipantes(data.participantes);
+                /*Banners*/
+                insertBannersEvento(data.banners);
+                /*etiqutas*/
+                insertEtiquetas(data.etiquetas);
+                /*patrocinadores*/
+                insertPatrocinadores(data.patrocinadores);
+                /*tematicas*/
+                insertTematicas(data.tematicas);
+                /*nomenclatura filtros*/
+                insertNomenclaturaMenu(data.nomenclaturaFiltro);
+                /*nomenclatura Actividad*/
+                insertNomenclaturaActividad(data.nomenclaturaActividad);
+                /*eventos solo sincro*/
+                insertEventosSincroIncial(data.eventos);
+                /*actividades solo sincro*/
+                insertActividadesSoloSincro(data.actividades);
+                /*relacion actividad etiquetas*/
+                insertActividadEtiquetaSincroInicial(data.actEti);
+                /*relacion actividad-participante*/
+                insertActividadParticipanteSincroInicial(data.actPart);
+                /*relacion actividad-tematica*/
+                insertActividadTematicaSincroInicial(data.actTematica);
+                /*relacion actividad-evento*/
+                insertActividadEventoSincroInicial(data.actEvento);
+                /**/
+                tengoSincro(true)
+            });
+        }, function (e){errorSincro(e)});
     }
+    PromesaSincro.then(function(isOk){
+        $("#canvasLoader").remove()
+    })
     $("#canvasLoader").remove()
 })()
 
-function sincroBorrados(){
+function sincroBorrados() {
     var auth = make_base_auth("app", "Kurbana2k14");
-        $.ajaxSetup({
-            headers: {
-                'Authorization': auth,
-            }
-        });
-        var jsNow = new Date ().getTime();
-        var ruta = sessionPath + "proyecto/getBorrados?id=" + sessionProyecto+"&fecha="+jsNow
-        $.getJSON(ruta, null, function (data) {
-            if(data.actividad.length>0){
-                deleteActividad(data.actividad)
-            }
-            if(data.evento.length>0){
-                deleteEventos(data.evento)
-            }
-            if(data.banner.length>0){
-                deleteBannerEvento(data.banner)
-            }
-            if(data.etiqueta.length>0){
-                deleteEtiquetas(data.etiqueta)
-            }
-            if(data.tematica.length>0){
-                deleteTematicas(data.tematica)
-            }
-            if(data.lugar.length>0){
-                deleteLugares(data.lugar)
-            }
-            if(data.participante.length>0){
-                deleteParticipantes(data.participante)
-            }
-            if(data.nomenclaturaActividad.length>0){
-                deleteNomenclaturaActividad(data.nomenclaturaActividad)
-            }
-            if(data.patrocinador.length>0){
-                deletePatrocinadores(data.patrocinador)
-            }
-            if(data.nomenclaturaFiltro.length>0){
-                deleteNomenclaturaMenu(data.nomenclaturaFiltro)
-            }
-        });
+    $.ajaxSetup({
+        headers: {
+            'Authorization': auth,
+        }
+    });
+    var jsNow = new Date().getTime();
+    var ruta = sessionPath + "proyecto/getBorrados?id=" + sessionProyecto + "&fecha=" + jsNow
+    $.getJSON(ruta, null, function (data) {
+        if (data.actividad.length > 0) {
+            deleteActividad(data.actividad)
+        }
+        if (data.evento.length > 0) {
+            deleteEventos(data.evento)
+        }
+        if (data.banner.length > 0) {
+            deleteBannerEvento(data.banner)
+        }
+        if (data.etiqueta.length > 0) {
+            deleteEtiquetas(data.etiqueta)
+        }
+        if (data.tematica.length > 0) {
+            deleteTematicas(data.tematica)
+        }
+        if (data.lugar.length > 0) {
+            deleteLugares(data.lugar)
+        }
+        if (data.participante.length > 0) {
+            deleteParticipantes(data.participante)
+        }
+        if (data.nomenclaturaActividad.length > 0) {
+            deleteNomenclaturaActividad(data.nomenclaturaActividad)
+        }
+        if (data.patrocinador.length > 0) {
+            deletePatrocinadores(data.patrocinador)
+        }
+        if (data.nomenclaturaFiltro.length > 0) {
+            deleteNomenclaturaMenu(data.nomenclaturaFiltro)
+        }
+    });
 }
